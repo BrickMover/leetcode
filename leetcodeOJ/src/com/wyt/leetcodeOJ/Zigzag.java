41eb2592edc2082994637547d1e450331d698cb7
wangyitao <wangyitaocp3@163.com>
2014-12-30 16:20:18 +0800

    Zigzag 添加注释
    N/A
    
    Change-Id: I2125383b94396282fe05f5de46a68f9e51198995
    Signed-off-by: wangyitao <wangyitaocp3@163.com>

--- leetcodeOJ/src/com/wyt/leetcodeOJ/Zigzag.java
+++ leetcodeOJ/src/com/wyt/leetcodeOJ/Zigzag.java
@@ -1,19 +1,43 @@
 package com.wyt.leetcodeOJ;
 
+/**
+ * @author wangyitao
+ * @version 1.0
+ */
 public class Zigzag {
 	
-	
+	/**
+	 * @param args
+	 * Input "123456789" 4
+	 * Output 
+	 *		  1      7
+	 *		  2    6 8
+	 *		  3  5   9
+	 *		  4
+	 * 
+	 * "172683594"
+	 */
 	public static void main(String[] args) {
 		System.out.println(convert("123456789", 4));
 	}
 	
 	
+	/**
+	 * @param s 待转换成之字形的字符串
+	 * @param nRows
+	 * @return
+	 */
 	public static String convert (String s, int nRows) {
 		
 		if(s == null || nRows <= 1) {
             return s;
         }
-		
+		/**
+		 * 思路：
+		 * 第一行代数关系：     				步长= 2x(rows - 1)  初始量0
+		 * 最后一行代数关系：				步长= 2x(rows - 1)  初始量rows - 1
+		 * 中间（1~rows-1）行代数关系：    	步长= 2x(rows - 1)  初始量 i ，结点值j+(rows-1-i)x2 不能超过字符串长度
+		 */
 		StringBuffer sb = new StringBuffer();
 		
 		for (int i = 0; i < s.length(); i += (nRows-1)*2) {
