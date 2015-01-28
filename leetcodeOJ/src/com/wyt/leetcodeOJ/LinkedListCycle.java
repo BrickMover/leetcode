b7620827d05cba6f5fb200ce4f7b790133b3e57e
wangyitao <wangyitaocp3@163.com>
2015-01-28 11:59:26 +0800

    提交检测链表环点的代码
    N/A
    
    Change-Id: I663e4e3d56a18ff816b40cf67a4a9a6403914bcc
    Signed-off-by: wangyitao <wangyitaocp3@163.com>

--- leetcodeOJ/src/com/wyt/leetcodeOJ/LinkedListCycle.java
+++ leetcodeOJ/src/com/wyt/leetcodeOJ/LinkedListCycle.java
@@ -13,14 +13,13 @@ package com.wyt.leetcodeOJ;
 public class LinkedListCycle {
 
 	public static void main(String[] args) {
-		ListNode head = new ListNode(1);
-		head.next = head;
+		ListNode head = new ListNode(3);
 		ListNode node1 = new ListNode(2);
-//		ListNode node2 = new ListNode(3);
-//		ListNode node3 = new ListNode(4);
-//		ListNode node4 = new ListNode(5);
-		head.next=node1;node1.next=head;//node2.next=node3;node3.next=node4;//node4.next=node2;
-		System.out.println(hasCycle(head));
+		ListNode node2 = new ListNode(0);
+		ListNode node3 = new ListNode(4);
+		//ListNode node4 = new ListNode(5);
+		head.next=node1;node1.next=node2;node2.next=node3;node3.next=node1;//node4.next=node2;
+		System.out.println(detectCycle(head).val);
 	}
 	
 	public static boolean hasCycle(ListNode head) {
@@ -30,10 +29,6 @@ public class LinkedListCycle {
         //创建两个指针，以每次走一步，一个每次走2步
 		ListNode p1 = head;//走一步
 		ListNode p2 = head.next;//走两步
-		
-		if (head.next == head) {
-			return true;
-		}
 		
 		while (p1 != null && p2 != null) {
 			if(p1 == p2) {//相遇，返回true
@@ -49,5 +44,47 @@ public class LinkedListCycle {
 		}
 		return false;
     }
+	
+	/**
+	 * @param head
+	 * @return
+	 * @Description
+	 * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+	 * Follow up:
+	 * Can you solve it without using extra space?
+	 * 
+	 * 设置第三个节点，和走一步的同步，先不走，当快的和慢的相遇，此时可以计算出起点到环点的距离等于相遇点到环点的距离
+	 * 所以此时第三个指针和走一步的指针同时开始走，相遇点即环点
+	 */
+	public static ListNode detectCycle(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+		
+		ListNode header = new ListNode(-1);
+		header.next = head;
+		ListNode p1 = header;
+		ListNode p1_3 = header;
+		ListNode p2 = header.next;
+		
+		while (p1 != null && p2 != null) {
+			if (p1 == p2) {
+				p1 = p1.next;
+				while (p1 != p1_3) {
+					p1 = p1.next;
+					p1_3 = p1_3.next;
+				}
+				return p1;
+			} else {
+				p1 = p1.next;
+				if (p2.next == null) {
+					return null;
+				} else {
+					p2 = p2.next.next;
+				}
+			}
+		}
+		return null;
+	}
 
 }
