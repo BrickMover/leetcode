867f92d71acdbe0e1d0ec190a6ea8fa2327b5dfe
wangyitao <wangyitaocp3@163.com>
2015-01-29 14:26:24 +0800

    提交Find Minimum Min Rotated Sorted Array II代码
    N/A
    
    Change-Id: I340b2fd64eb6dc780ede3796dc655d8ac46ed646
    Signed-off-by: wangyitao <wangyitaocp3@163.com>

--- leetcodeOJ/src/com/wyt/leetcodeOJ/FindMinimuminRotatedSortedArray.java
+++ leetcodeOJ/src/com/wyt/leetcodeOJ/FindMinimuminRotatedSortedArray.java
@@ -14,8 +14,8 @@ package com.wyt.leetcodeOJ;
 public class FindMinimuminRotatedSortedArray {
 
 	public static void main(String[] args) {
-		int[] num = {7,8,9,4,5,6,0,1,2};
-		System.out.println(findMin(num));
+		int[] num = {7,8,8,9,3,3,4,5,1,2,3};
+		System.out.println(findMinII(num));
 	}
 
 	/**
@@ -48,4 +48,38 @@ public class FindMinimuminRotatedSortedArray {
 		}
         return result;
     }
+	
+	/**
+	 * @param num
+	 * @return
+	 * @Description
+	 * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+	 * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+	 * Find the minimum element.
+	 * The array may contain duplicates.
+	 */
+	public static int findMinII (int[] num) {
+		if (num.length == 1) {
+        	return num[0];
+        }
+        if (num.length == 2) {
+			return num[0] >= num[1] ? num[1] : num[0];
+		}
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < num.length; i++) {
+        	if (i==0 && num[i] <= num[i+1]) {//加上=号
+				result = num[i];
+			}
+        	if (i == num.length - 1) {
+				if (num[i] <= num[i-1] && num[i]<result) {//加上=号
+					result = num[i];
+				}
+				break;
+			}
+			if (i != 0 && num[i] <= num[i-1] && num[i] <= num[i+1] && num[i] < result) {//加上=号
+				result = num[i];
+			}
+		}
+        return result;
+	}
 }
